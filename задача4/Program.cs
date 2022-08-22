e7d87a9ccfaab17a869d41a7f6540f6b4e2c0238
Olga <olyakapran@yandex.by>
2022-08-22 15:44:12 +0300

    first commit

---
+++ задача4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace задача4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int num1 = 2;
+            while ( num1<=num)
+            { 
+                Console.Write(num1+", ");
+                num1 +=2;
+            } 
+
+        }
+    }
+}
